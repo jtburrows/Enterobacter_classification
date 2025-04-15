83120a7a78f4cd667ff39f8a0d6cac7b9efac898
jtburrows <jtburrow@ucsd.edu>
2025-04-15 01:19:06 -0700

    Add snakemake workflow and scripts to build P matrix and Mash distances

---
+++ Scripts/combine_P_matrix.py
@@ -0,0 +1,40 @@
+import sys
+import pandas as pd
+from collections import defaultdict
+
+def main(argv):
+    input_file = argv[1]
+    output_file = argv[2]
+
+    P_matrix = pd.read_pickle('Data/Ebacter_strain_by_gene.pickle')
+    metadata = pd.read_csv('Data/metadata.csv', dtype = 'object').set_index('genome_id')
+    
+    new_P_matrix = defaultdict(dict)
+    with open(input_file, 'r') as f:
+        for line in f:
+            input_name = line.split('.clstr')[0].split('/')[2]
+            with  open(line.strip(), 'r') as infile:
+                cluster = ''
+                present = False
+                
+                for l in infile:
+                    if l[0] == '>':
+                        if cluster != '':
+                                new_P_matrix[cluster][input_name] = int(present)
+                        cluster = ''
+                        present = False
+                    else:
+                        if input_name not in l:
+                            cluster = l.split('>')[1].split('...')[0]
+                        if input_name in l:
+                            present = True
+                
+                if cluster != '':
+                    new_P_matrix[cluster][input_name] = int(present)
+
+    new_P_matrix = pd.DataFrame(new_P_matrix).T
+    new_P_matrix.to_csv('Work/combined_P_matrix.csv')
+            
+
+if __name__ == "__main__":
+   main(sys.argv)
---
+++ Scripts/combine_mash.py
@@ -0,0 +1,21 @@
+import sys
+import pandas as pd
+def main(argv):
+    input_file = argv[1]
+    output_file = argv[2]
+
+    metadata = pd.read_csv('Data/metadata.csv', dtype = 'object').set_index('genome_id')
+    
+    mash_matrix = pd.DataFrame(index = metadata.index)
+    with open(input_file, 'r') as f:
+        for line in f:
+            input_name = line.split('.txt')[0].split('/')[2]
+            infile = pd.read_csv(line.strip(), sep='\t', header=None, dtype='object')
+            infile['pangenome_ids'] = infile.iloc[:,0].apply(lambda x: x.split('/')[-1][:-4])
+            infile = infile[infile.pangenome_ids.isin(metadata.index)]
+            mash_matrix[input_name] = infile.set_index('pangenome_ids')[2]
+            
+    mash_matrix.to_csv(output_file)
+
+if __name__ == "__main__":
+   main(sys.argv)
